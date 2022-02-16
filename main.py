192f7918bbf2b3125cdd4eb4b53d5fe3390f2e3e
Mika <fin_boy@mail.ru>
2022-02-16 19:03:09 +0300

    Cоздание базы данных(чтобы не создавать кучу переменных решил создать базу)

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import os
 import time
+import sqlite3
+
 
 
 def main_menu():
     start = True
     while True:
         if start:
-            num_menu = input("\tДобрый день уважаемый гость.\n\tВас приветствует ресторан быстрого питания.\n\
-            Чего желаете?\n\
-            Введите 1 чтобы сделать заказ.\n\
-            Введите 2 чтобы посмотреть на свой заказ.\n\
-            Введите 3 чтобы оплатить свой заказ.\n\
-                         ")
+            num_menu = input("\tДобрый день уважаемый гость.\n\tВас приветствует ресторан быстрого питания.\n"
+                             "\t\tЧего желаете?\n"
+                             "\tВведите 1 чтобы сделать заказ.\n"
+                             "\tВведите 2 чтобы посмотреть на свой заказ.\n"
+                             "\tВведите 3 чтобы оплатить свой заказ.\n")
         else:
-            num_menu = input("\
-            Выберите дальнейшие действия:\n\
-            Введите 1 чтобы изменить заказ.\n\
-            Введите 2 чтобы посмотреть на свой заказ.\n\
-            Введите 3 чтобы оплатить свой заказ.\n\
-                         ")
+            num_menu = input("\t\tВыберите дальнейшие действия:\n"
+                             "\tВведите 1 чтобы изменить заказ.\n"
+                             "\tВведите 2 чтобы посмотреть на свой заказ.\n"
+                             "\tВведите 3 чтобы оплатить свой заказ.\n")
 
         if num_menu == "1":
             start = False
@@ -47,14 +46,70 @@ def ordering_food():
     print("\t\t Введите номер желаемого.")
     while True:
         num_food = input('\
-            1) Пицца\n\
-            2) Бургеры\n\
-            3) Картошка\n\
-            4) Наггетсы\n\
-            5) Мороженое\n\
-            6) Напитки\n\
+            1) Аппетитные Бургеры\n\
+            2) Картошка фри\n\
+            3) Жаренные наггетсы\n\
+            4) Вкусное мороженое\n\
+            5) Освежающие напитки.\n\
+            6) Выйти из меню заказа.\n\
                          ')
+
+        if num_food == "1":
+            print(1)
+        elif num_food == "2":
+            print(2)
+        elif num_food == "3":
+            print(3)
+        elif num_food == "4":
+            print(4)
+        elif num_food == "5":
+            print(5)
+        elif num_food == "6":
+            break
+        else:
+            print("Введено неверное значение.")
+
+
+
+
+
+
+class DataBase:
+    @staticmethod
+    def create_table():
+        connect = sqlite3.connect('food_base')
+        sql = connect.cursor()
+        sql.execute("""CREATE TABLE IF NOT EXISTS food (
+            name_food TEXT,
+            count_food INT,
+            PRICE INT
+        )""")
+        connect.commit()
+        connect.close()
+
+    @staticmethod
+    def read(select):
+        conn = sqlite3.connect('food_base')
+        sql = conn.cursor()
+        sql.execute(select)
+        result = sql.fetchall()
+        sql.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def insert(name,count,price):
+        conn = sqlite3.connect('food_base')
+        sql = conn.cursor()
+        #user_data = DataBase.read(f"SELECT user_id FROM users WHERE user_id = '{message.from_user.id}'")
+        #if not user_data:
+        sql.execute("INSERT INTO food VALUES (?, ?, ?)",
+                        (name, count, price))
+        conn.commit()
+        sql.close()
+        conn.close()
 
 
 if __name__ == '__main__':
-    main_menu()
+    #main_menu()
+    DataBase.create_table()
